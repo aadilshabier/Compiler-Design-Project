8166171851f04163e9e9d0d6fa4a70d5dc565ab0
Mohammad Aadil Shabier <aadilshabier1@gmail.com>
2023-09-10 23:12:40 +0530

    Added second test case

--- test/prog1.c
+++ test/prog1.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
-int var1 = 4;
-float var2 = -2.78;
+
+int var1 = 4; // this shouldnt be read
+float var2 = -2.78; /*this
+					  either*/
 char c = '1';
+
+float fexp = 23.0e-12;
 
 int main() {
 	printf("Hello, World: %d\n", var1);
---
+++ test/prog2.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+/* This should print an error
+
+int main () {
+}
