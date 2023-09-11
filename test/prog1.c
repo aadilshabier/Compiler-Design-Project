8cc66cbda89014a11d6dac068100b80ad0f67ad6
Mohammad Aadil Shabier <aadilshabier1@gmail.com>
2023-09-11 15:20:41 +0530

    Write patterns and tests for characters

--- test/prog1.c
+++ test/prog1.c
@@ -5,7 +5,10 @@
 int var1 = 4; // this shouldnt be read
 float var2 = -2.78; /*this
 					  either*/
-char c = '1';
+char c1 = '1';
+char c2 = '\r';
+char c3 = '\\';
+char c4 = '\'';
 
 float fexp = 23.0e-12;
 
