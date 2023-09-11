decce9565f7d134eebb9d6c73424174a25add1ae
bleasey <calvin7ferns@gmail.com>
2023-09-11 16:19:52 +0530

    Changed test cases

--- test/prog1.c
+++ test/prog1.c
@@ -3,14 +3,6 @@
 #define MY_DEF
 
 int var1 = 4; // this shouldnt be read
-float var2 = -2.78; /*this
-					  either*/
-char c1 = '1';
-char c2 = '\r';
-char c3 = '\\';
-char c4 = '\'';
-
-float fexp = 23.0e-12;
 
 int main() {
 	printf("Hello, \tWorld: %d\n", var1 and 4);
--- test/prog3.c
+++ test/prog3.c
@@ -1,3 +1,5 @@
+#include<stdio.h>
+
 // This should fail
 
 int abcdefghijklmnopqrstuvwxyz123456;
