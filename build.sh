b3b9ee06e4f05fe4e612413001aebde30f538127
Mohammad Aadil Shabier <aadilshabier1@gmail.com>
2023-10-17 23:09:23 +0530

    Fix build script filename error

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #! /usr/bin/env sh
 
-set -xe
-
 # Check if an argument was provided
 if [ $# -lt 1 ]; then
     echo "Usage: ./build.sh <test_file_name.c>"
     exit 1
 fi
 
+
+set -xe
+
 flex++ lexer.l
 c++ lex.yy.cc
-./a.out ./test/$1
+./a.out $1
